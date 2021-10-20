9af7c220b7e79d7786f0aecde2e0e7a78781486e
Alessandro2014 <alessandromerenda22@gmail.com>
2021-10-20 17:33:00 +0200

    controller api

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -27,6 +27,7 @@
                     <th scope="col">#</th>
                     <th scope="col">Titotlo</th>
                     <th scope="col">Scritto il </th>
+                    <th scope="col" class="text-center">Opzioni</th>
                 </tr>
             </thead>
             <tbody>
@@ -35,7 +36,7 @@
                         <th scope="row">{{ $post->id }} </th>
                         <td>{{ $post->title }}</td>
                         <td>{{ $post->getFormattedDate('created_at', 'H:i d-m-Y') }}</td>
-                        <td class="text-right">
+                        <td class="text-center">
                             <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-info p-2">Dettaglio</a>
                             <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-secondary p-2">Modifica</a>
                             <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST"
