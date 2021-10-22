b2dfdafb25715908de09fd1a4afcd450d07b0e56
Alessandro2014 <alessandromerenda22@gmail.com>
2021-10-22 15:02:34 +0200

    print categories

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -11,11 +11,15 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
                         <p class="card-text">{{ $post->content }}</p>
-                        <p class="card-text"><small
-                                class="text-muted">{{ $post->getFormattedDate('created_at', 'H:i d-m-Y') }}</small></p>
+                        <p class="card-text">
+                            <small class="text-muted">{{ $post->getFormattedDate('created_at', 'H:i d-m-Y') }}</small>
+                            <span>
+                            @if ($post->category) {{ $post->category->name }} @else
+                                    nessuna categoria
+                            </span>
+                        </p>
                         <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-secondary p-2">Modifica</a>
                         <a href="{{ route('admin.posts.index') }}" class="btn btn-light">Indietro</a>
-
                     </div>
                 </div>
             </div>
